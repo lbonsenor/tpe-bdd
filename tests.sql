7b2f4d87ac848278315158236392ac9a28308984
Ana Negre <98236062+anita476@users.noreply.github.com>
2024-11-16 11:09:36 -0300

    delete comments

--- tests.sql
+++ tests.sql
@@ -3,13 +3,11 @@
 
 CREATE OR REPLACE FUNCTION test() RETURNS VOID AS $$
 DECLARE
-    eq RECORD;  -- This will hold each team (equipo) value.
-    player RECORD;  -- This will hold each player's details (jugador, posicion, dorsal)
+    eq RECORD;  
+    player RECORD; 
 BEGIN
-    -- Loop over each distinct team
     FOR eq IN (SELECT DISTINCT equipo FROM futbolista_prueba) 
     LOOP
-        -- Get player details for the current team (equipo)
         FOR player IN 
             SELECT dp.jugador, fp.posicion, dp.dorsal
             FROM dorsal_prueba dp
