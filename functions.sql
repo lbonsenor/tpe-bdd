f4308d30808961f4da834af542939138486f84c8
Lautaro Bonseñor <lbonsenor@itba.edu.ar>
2024-11-18 15:38:30 -0300

    technically not a parasite :D

--- functions.sql
+++ functions.sql
@@ -1,7 +1,41 @@
-DROP TABLE futbolista CASCADE;
+DROP TABLE IF EXISTS futbolista CASCADE;
 
-DROP TABLE dorsal CASCADE;
+DROP TABLE IF EXISTS dorsal CASCADE;
+-- Para ver por equipos el dorsal asignado
+CREATE
+OR REPLACE FUNCTION test() RETURNS VOID AS $$ DECLARE eq RECORD;
 
+player RECORD;
+
+BEGIN FOR eq IN (
+    SELECT
+        DISTINCT equipo
+    FROM
+        futbolista
+) LOOP FOR player IN
+SELECT
+    dp.jugador,
+    fp.posicion,
+    dp.dorsal
+FROM
+    dorsal dp
+    JOIN futbolista fp ON fp.nombre = dp.jugador
+WHERE
+    fp.equipo = eq.equipo
+ORDER BY
+    dp.dorsal LOOP RAISE NOTICE 'Equipo: %, Jugador: %, Posicion: %, Dorsal: %',
+    eq.equipo,
+    player.jugador,
+    player.posicion,
+    player.dorsal;
+
+END LOOP;
+
+END LOOP;
+
+END;
+
+$$ LANGUAGE plpgsql;
 -- El predeterminado es MDY
 SET datestyle = 'DMY';
 
